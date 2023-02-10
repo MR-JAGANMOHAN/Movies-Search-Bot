d04c8e4b0e7e5a9aca3db294ed3bc2924ece69e1
MR-JAGANMOHAN <107793234+MR-JAGANMOHAN@users.noreply.github.com>
2023-02-10 20:42:11 +0530

    Update index.py

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ from movies_scraper import search_movies, get_movie
 
 
 TOKEN = os.getenv("TOKEN")
-URL = os.getenv("URL")
+URL = "https://vercel.com/jaganmohan9110-gmailcom/movies-search-bot"
 bot = Bot(TOKEN)
 
 
